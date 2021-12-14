bd9072daf898b77dae80ef8d047c3d23c8073caf
unknown <Administrador@dbserver01.terati.com>
2021-12-14 16:02:52 -0300

    editando model categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,15 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
+import { Category } from "./pages/categories/shared/category.model";
+
 export class InMemoryDataBase implements InMemoryDbService {
     createDb(){
-        const categories = [
-            { id: 1, nome: 'Moradia', description: "Pagamentos de contas da casa" },
-            { id: 1, nome: 'Saúde', description: "Plano de saúde e remédios" },
-            { id: 1, nome: 'Lazer', description: "Cinema, parques, praia, etc" },
-            { id: 1, nome: 'Salário', description: "Recebimento de salário" },
-            { id: 1, nome: 'Freelas', description: "Trabalhos com freelancer" }
+        const categories: Category[] = [
+            { id: 1, name: 'Moradia', description: "Pagamentos de contas da casa" },
+            { id: 1, name: 'Saúde', description: "Plano de saúde e remédios" },
+            { id: 1, name: 'Lazer', description: "Cinema, parques, praia, etc" },
+            { id: 1, name: 'Salário', description: "Recebimento de salário" },
+            { id: 1, name: 'Freelas', description: "Trabalhos com freelancer" }
         ];
 
         return { categories }
