94d76aac40e6b5ec9828e530a2c542ff781229b1
unknown <Administrador@dbserver01.terati.com>
2021-12-17 15:54:39 -0300

    ordenando os lancamentos

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,3 +1,4 @@
+import { CategoryService } from './../../categories/shared/category.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -5,6 +6,7 @@ import { Observable, throwError } from 'rxjs';
 import { flatMap, catchError, map } from 'rxjs/operators';
 
 import { Entry } from './entry.model';
+import { Category } from '../../categories/shared/category.model';
  
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,10 @@ export class EntryService {
 
   private apiPath: string = "api/entries"
   
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService
+  ) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -32,19 +37,32 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-        catchError(this.handleError),
-        map(this.jsonDataToEntry)
-      )
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category: Category) => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        )
+      })
+    );
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
 
-    return this.http.put(url, entry).pipe(
-        catchError(this.handleError),
-        map(() => entry)
-    )
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap((category: Category) => {
+        entry.category = category;
+        
+        return this.http.put(url, entry).pipe(
+            catchError(this.handleError),
+            map(() => entry)
+        )
+      })
+    );
+
   }
 
   delete(id: number): Observable<any> {
