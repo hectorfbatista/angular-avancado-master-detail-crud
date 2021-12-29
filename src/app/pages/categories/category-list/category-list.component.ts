683cbcfe512e26d005b0a190f866b91ea1ecbfcd
unknown <Administrador@dbserver01.terati.com>
2021-12-29 15:33:39 -0300

    aprimorando category list com o baseresourcecomponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
@@ -8,27 +9,10 @@ import { CategoryService } from '../shared/category.service';
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
-  categories: Category[] = [];
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-  constructor(private categoryService: CategoryService) { }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-      error => alert('Erro ao carregar a lista')
-    )
-  }
-
-  deleteCategory(category: Category) {
-    const mustDelete = confirm('Deseja realmente excluir esse item?');
-
-    if (mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => this.categories = this.categories.filter(element => element != category),
-        () => alert('Erro ao tentar excluir categoria')
-      );
-    }
+  constructor(private categoryService: CategoryService) { 
+    super(categoryService);
   }
 
 }
