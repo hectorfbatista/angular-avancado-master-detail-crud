39470a399bfee169ab7d94ec81ac69dac7829fae
unknown <Administrador@dbserver01.terati.com>
2021-12-29 15:37:19 -0300

    aprimorando entry-list com o baseresourcecomponent

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
+import { Component } from '@angular/core';
 
 import { Entry } from '../shared/entry.model';
 import { EntryService } from '../shared/entry.service';
@@ -8,27 +9,10 @@ import { EntryService } from '../shared/entry.service';
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.css']
 })
-export class EntryListComponent implements OnInit {
-  entries: Entry[] = [];
+export class EntryListComponent extends BaseResourceListComponent<Entry> {
 
-  constructor(private entryService: EntryService) { }
-
-  ngOnInit() {
-    this.entryService.getAll().subscribe(
-      entries => this.entries = entries.sort((a,b) => b.id - a.id),
-      error => alert('Erro ao carregar a lista')
-    )
-  }
-
-  deleteEntry(entry: Entry) {
-    const mustDelete = confirm('Deseja realmente excluir esse item?');
-
-    if (mustDelete) {
-      this.entryService.delete(entry.id).subscribe(
-        () => this.entries = this.entries.filter(element => element != entry),
-        () => alert('Erro ao tentar excluir categoria')
-      );
-    }
+  constructor(private entryService: EntryService) { 
+    super(entryService);
   }
 
 }
